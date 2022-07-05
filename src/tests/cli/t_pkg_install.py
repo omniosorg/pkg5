f4447eebbbccac4657931f835825661373ebe6c9
Darren J Moffat <Darren.Moffat@Oracle.COM>
2022-07-05 06:49:42 -0700

    33845953 constrained image puts auto-update into maintenance

--- src/tests/cli/t_pkg_install.py
+++ src/tests/cli/t_pkg_install.py
@@ -22,7 +22,7 @@
 #
 
 #
-# Copyright (c) 2008, 2020, Oracle and/or its affiliates.
+# Copyright (c) 2008, 2022, Oracle and/or its affiliates.
 # Copyright 2021 OmniOS Community Edition (OmniOSce) Association.
 #
 
@@ -2090,7 +2090,7 @@ class TestPkgInstallUpdateReject(pkg5unittest.SingleDepotTestCase):
                 self.pkg("list -Hv kernel@1.0,5.11-0.1 | /usr/bin/awk '{print $1}'")
                 kernel_fmri = self.output
                 # upgrade to next version w/o encountering idrs
-                self.pkg("update -v");
+                self.pkg("update -v")
                 self.pkg("list kernel@1.0,5.11-0.2")
                 self.pkg("list")
 
@@ -7736,7 +7736,7 @@ class TestDependencies(pkg5unittest.SingleDepotTestCase):
                 # test to see if solver will fail gracefully when no solution is
                 # possible and a require-any dependency is involved
                 self.pkg("install -vvv pkg-nosol-A pkg-nosol-E",
-                    assert_solution=False, exit=1)
+                    assert_solution=False, exit=9)
 
                 # test to see if solver will pick one
                 self.pkg("install pkg8@1.0")  # install pkg
@@ -7785,7 +7785,7 @@ class TestDependencies(pkg5unittest.SingleDepotTestCase):
                 # Test to see if solver will fail gracefully when no solution is
                 # possible and a require-any dependency is involved.
                 self.pkg("exact-install -v pkg-nosol-A pkg-nosol-E",
-                    assert_solution=False, exit=1)
+                    assert_solution=False, exit=9)
 
                 # Test to see if solver will pick one.
                 self.pkg("exact-install pkg8@1.0")
